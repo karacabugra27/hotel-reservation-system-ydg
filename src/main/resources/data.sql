8b4b38ff037337dee6a39bb591120713957115d2
Buğrahan Karaca <karacabugra27@gmail.com>
2025-12-22 01:06:11 +0300

    jenkinsfile

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 -- ROOM TYPE SEED
 INSERT INTO room_type (id, name, capacity, base_price)
 VALUES
-    (1, 'Single', 1, 1000),
-    (2, 'Double', 2, 1500);
+    ( 'Single', 1, 1000),
+    ( 'Double', 2, 1500);
 
 -- ROOM SEED
 INSERT INTO room (id, room_number, room_type_id, room_status)
 VALUES
-    (1, '101', 1, 'AVAILABLE'),
-    (2, '102', 2, 'AVAILABLE'),
-    (3, '103', 1, 'OCCUPIED');
+    ( '101', 1, 'AVAILABLE'),
+    ('102', 2, 'AVAILABLE'),
+    ( '103', 1, 'OCCUPIED');
