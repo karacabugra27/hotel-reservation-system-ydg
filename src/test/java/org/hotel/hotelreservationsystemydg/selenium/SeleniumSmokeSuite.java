bd85a382965deb03cca9340dccc4e16b8676a091
Buğrahan Karaca <karacabugra27@gmail.com>
2026-01-08 14:56:52 +0300

    deneme

--- src/test/java/org/hotel/hotelreservationsystemydg/selenium/SeleniumSmokeSuite.java
+++ src/test/java/org/hotel/hotelreservationsystemydg/selenium/SeleniumSmokeSuite.java
@@ -2,7 +2,7 @@ package org.hotel.hotelreservationsystemydg.selenium;
 
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
-
+//deneme
 @Suite
 @SelectClasses({
         HomePageSeleniumTest.class,
